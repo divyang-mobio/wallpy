516fba45cecf64797f060d7542b0b984cf9146b8
divyang-mobio <107481877+divyang-mobio@users.noreply.github.com>
2022-08-18 17:03:41 +0530

    Update color_resources.dart

--- lib/resources/color_resources.dart
+++ lib/resources/color_resources.dart
@@ -30,9 +30,5 @@ class ColorResources {
   final Color newsDetailText = Colors.black;// Colors.white;
   final Color categoriesTileBorder = Colors.black;// Colors.white;
   final Color activeSwitch = Colors.black;// Colors.white;
-  final Color googleSignInUpText = Colors.white;
-  final Color search = Colors.black;
-  final Color logOut = Colors.black;
-  final Color signUpInText = Colors.black;
   final Color categoryColor = const Color.fromARGB(30, 0, 0, 0);
 }
